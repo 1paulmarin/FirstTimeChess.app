7a4b6bbd3b757584dd554d86ec44ee374cada6ab
1paulmarin <paulmarin663@gmail.com>
2025-08-25 22:06:57 +0300

    Remove middleware to fix Vercel routing error

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-// Simple middleware that does nothing - no authentication required
-export function middleware() {
-  // No middleware processing needed for simple chess app
-  return
-}
-
-export const config = {
-  matcher: [],
-}
